0a7bf7942910edd2a89969eb7942c59dbc55673f
Felipe <felipedossantosflor@gmail.com>
2022-09-08 11:03:49 -0300

    @FelipeFlor [Task: First exercise]

---
+++ Exercise1/Exercise1/Exercise1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Exercise1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int indice = 13;
+            int soma = 0;
+            int k = 0;
+
+            while (k < indice)
+            {
+                k = k + 1;
+                soma = soma + 1;
+            }
+            Console.WriteLine($"The value of soma is {soma}");
+        }
+    }
+}
---
+++ Exercise1/Exercise1/Exercise2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Exercise2
+{
+    internal class Fibonacci
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, please type your number between 0 and 1000 to verify is it's part of the Fibonacci sequence: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+
+            for (int i = 0; i < number; i++)
+            {
+                
+
+
+            }
+        }
+    }
+}
